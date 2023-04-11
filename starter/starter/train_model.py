6dc461af1de9b6c8208ddb04eec3d8f73cf5c5a6
Jonathan Ratschat <jonathan.ratschat@ing.com>
2023-04-11 10:21:42 +0200

    Enable model training

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,15 @@
 # Script to train machine learning model.
+from pathlib import Path
 
-from sklearn.model_selection import train_test_split
+import pandas as pd
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import compute_model_metrics, inference, save_model, train_model
+from sklearn.model_selection import train_test_split
 
 # Add code to load in the data.
+data = pd.read_csv(Path().cwd() / "starter/data/census_cleaned.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,10 +24,33 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb,
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+save_model(model, Path().cwd() / "starter/model/model.sav")
+
+# Test model and print evaluation metrics
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(
+    f"""
+    ##### Model performance #####
+    Precision: {round(precision, 4)}
+    Recall: {round(recall, 4)}
+    Fbeta: {round(fbeta, 4)}
+    """
+    )
